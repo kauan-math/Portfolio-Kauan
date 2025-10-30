45bf0e6ff34ed65c8cdce8cd4f2078c3df5cabac
Kauan Matheus Sanche Corrêa <kauan.math.correa@gmail.com>
2025-10-30 10:03:31 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import styles from './App.module.css'
+
+function App() {
+
+//javascript é em cima do return
+
+  return (
+    <>
+      <h1 className={styles.titulo}>teste</h1>
+    </>
+  )
+}
+
+export default App
