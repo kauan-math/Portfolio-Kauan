bf41a099318bcaab18b22c4eee6daecf6ec0eaca
Kauan Matheus Sanche Corrêa <kauan.math.correa@gmail.com>
2025-11-03 11:29:43 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import styles from './App.module.css'
+import perfil from '../public/foto3.jpg'
 
 function App() {
 
-//javascript é em cima do return
+  //javascript é em cima do return
 
   return (
     <>
@@ -13,19 +14,24 @@ function App() {
         <a href="#s4">Contato</a>
       </nav>
       <main>
-      <section>
-        <h2>sessão 1</h2>
-      </section>
-      <section>
-        <h2>sessão 2</h2>
+        <section className={styles.s1} id='s1'>
+          <div className={styles.left}>
+            <img className={styles.imgPerfil} src={perfil} alt="Foto" />
+          </div>
+          <div className={styles.right}>
+            <h2 className={styles.title}>Kauan Matheus</h2>
+            <p className={styles.paragraph}>Programador Iniciante, estudo no Primeiro Ano do Ensino Médio, Cursando um curso Técnico em Desenvolvimento de Sistemas no Senai</p>
+          </div>
         </section>
-      <section>
-        <h2>sessão 3</h2>
-      </section>
-      <section>
-        <h2>sessão 4</h2>
+        <section id='s2'>
+          <h2>sessão 2</h2>
         </section>
-
+        <section id='s3'>
+          <h2>sessão 3</h2>
+        </section>
+        <section id='s4'>
+          <h2>sessão 4</h2>
+        </section>
       </main>
     </>
   )
