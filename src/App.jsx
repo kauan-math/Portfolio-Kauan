29e3bed9b0f519a0cbf0b35117c9f1c8360ac75c
Kauan Matheus Sanche Corrêa <kauan.math.correa@gmail.com>
2025-11-11 11:44:28 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import styles from './App.module.css'
+
 import perfil from '../public/foto3.jpg'
 import facebookIcon from '../public/facebook.png'
 import instagramIcon from '../public/instagram.png'
@@ -11,9 +12,33 @@ import viteImg from '../public/vite.png'
 import reactImg from '../public/React.png'
 import vercelImg from '../public/Vercel.png'
 
+import { useState } from 'react'
+
 function App() {
 
-  //javascript é em cima do return
+  const defaultPhoneNumber = '554192897222'
+
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  })
+
+  const handleChange = (event) => {
+    const { name, value } = event.target
+    setFormData({ ...formData, [name]: value })
+  }
+
+  const handleZap = () => {
+    const { name, email, message } = formData
+
+    const urlzap = `https://api.whatsapp.com/send?phone=${defaultPhoneNumber}&text=
+    Nome:%20${name}%0D%0A
+    Email:%20${email}%0D%0A
+    Mensagem:%20${message}%0D%0A`
+
+    window.open(urlzap, "_blank")
+  }
 
   return (
     <>
@@ -37,7 +62,7 @@ function App() {
           <h2 className={styles.tecTitle}>Tecnologias</h2>
           <div className={styles.wrapCards}>
             <div className={styles.card}>
-              <img width={100}  src={htmlimg} alt="HTML" />
+              <img width={100} src={htmlimg} alt="HTML" />
               <h3>HTML</h3>
               <p>HTML é a sigla para Hypertext Markup Language (Linguagem de Marcação de Hipertexto) e é a linguagem de marcação padrão usada para criar e estruturar páginas da web</p>
             </div>
@@ -52,7 +77,7 @@ function App() {
               <p>JavaScript é uma linguagem de programação leve, interpretada e versátil, usada para tornar páginas da web dinâmicas e interativas</p>
             </div>
             <div className={styles.card}>
-              <img width={100} src={viteImg} alt="Vite"/>
+              <img width={100} src={viteImg} alt="Vite" />
               <h3>Vite JS</h3>
               <p>Vite JS é uma ferramenta moderna de construção de front-end que proporciona uma experiência de desenvolvimento mais rápida e eficiente para projetos web.</p>
             </div>
@@ -67,13 +92,39 @@ function App() {
               <p>Vercel é uma plataforma de nuvem que facilita o desenvolvimento e a implantação de sites e aplicativos web modernos.</p>
             </div>
           </div>
-          
         </section>
-        <section id='s3'>
-          <h2>sessão 3</h2>
+        <section id='s3' className={styles.s3}>
+          <h2>Github repositorios</h2>
+          <p>sadas</p>
         </section>
-        <section id='s4'>
-          <h2>sessão 4</h2>
+
+        <section id='s5' className={styles.s5}>
+
+          <div class="card">
+  <div class="card-content">
+    <p class="card-title">Card hover effect
+    </p><p class="card-para">Lorem ipsum dolor sit 
+      amet, consectetur adipiscing elit.</p>
+ 
+
+
+          <h2>Minha Escola</h2>
+          <p>Minha escola, mesmo com seus problemas, ainda é uma escola muito boa</p>
+        </div> 
+        </div>  
+        </section>
+
+        <section id='s4' className={styles.s4}>
+          <h2>CONTATO</h2>
+          <div className={styles.formData}>
+            <label htmlFor="name">Informe seu nome</label>
+            <input type="text" id='name' name='name' value={formData.name} onChange={handleChange} required />
+            <label htmlFor="email">Informe seu email</label>
+            <input type="email" name='email' id='email' value={formData.email} onChange={handleChange} required />
+            <label htmlFor="message">Informe uma mensagem</label>
+            <textarea name="message" id="message" value={formData.message} onChange={handleChange} cols="30" rows="10" required></textarea>
+            <button onClick={handleZap}>Enviar mensagem</button>
+          </div>
         </section>
       </main>
       <footer className={styles.rodape}>
@@ -85,5 +136,4 @@ function App() {
     </>
   )
 }
-
 export default App
